0f076ff762ed34d01a2f74b8f0723451d53d4d9a
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2023-03-29 01:46:55 +0300

    тип егэ2:

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+using namespace std;
+
+void type2() {
+	int x, y, w, z;
+	int go(int x=0); {
+		int go(int y=0); {
+			int go(int w=0); {
+				int go(int z=0); {
+					if (x >= 2) {
+						exit;
+					}
+					else if  ((x || !y) && !(w == z) && w) {
+						cout << x << " " << y << " " << z << " " << w << endl;
+						x++;
+						y++;
+						w++;
+						z++;
+						exit;
+					}
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	type2();
+}
+
+
