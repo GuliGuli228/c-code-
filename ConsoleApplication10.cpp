8e5242c542fdc0fdd69a835cc4c6400f1449d75d
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2023-03-29 02:03:32 +0300

    ege

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x = 0, y = 0, w = 0, z = 0;
+	cout << "x y w z";
+	while (true) {
+		if (x >= 2|| y >= 2|| w >= 2|| z >= 2) {
+			break;
+		}
+		if (((x || !y) && !(w == z) && w)) {
+			cout << x << " " << y << " " << z << " " << w << endl;
+			x++;
+			y++;
+			w++;
+			z++;
+		}
+	}
+	return 0;
+}
+
+
